2d548d9c32daab96a2b53f0057e514a5c78ed22b
LDominik1 <10c-lality@ipari.vein.hu>
2024-11-13 19:04:01 +0100

    proba

--- fooldal.php
+++ fooldal.php
@@ -17,9 +17,7 @@
         include './navbar.php';
     ?>
 
-    <div class="">
 
-    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
