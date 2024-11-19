a791008d9bca5e99808cc1eb3613a346c1b27c72
Halasi Martin <13c-halasi@IPARI.SULINET.HU>
2024-11-19 10:10:27 +0100

    input width vissza a helyere, font family egyesites

--- profil.php
+++ profil.php
@@ -8,28 +8,24 @@ if (!isset($_SESSION['felhasznalo'])) {
     exit();
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&family=Kanit:wght@300&family=Montserrat&family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./style/style.css">
-    <title>HaLáli Webshop</title>
+    <title>Termékek</title>
 </head>
-<style>
-    
-</style>
 <body>
+
     <?php
         include './navbar.php';
     ?>
-    <div id="profilDolog">
-
-    </div>
     <h2>Üdvözöljük, <?php echo htmlspecialchars($_SESSION['felhasznalo']); ?>!</h2>
+    
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
