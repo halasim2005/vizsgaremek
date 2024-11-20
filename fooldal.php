8ff534093fe8b87773ba917d3bcd0e1f507baac7
LDominik1 <10c-lality@ipari.vein.hu>
2024-11-20 19:02:31 +0100

    fooldal bemutatkozas, termekek div, css design

--- fooldal.php
+++ fooldal.php
@@ -13,6 +13,9 @@ header("Pragma: no-cache");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&family=Kanit:wght@300&family=Montserrat&family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./style/style.css">
     <title>HaLáli Webshop</title>
 </head>
@@ -30,9 +33,24 @@ header("Pragma: no-cache");
                 <td width="50%">
                     <img id="logoKepBemutatkozasDiv" src="./képek/HaLálip.png" alt="HaLáli Villszer Kft. logo">
                 </td>
-                <td width="50%" class="bemutatkozoTableTd">
-                    <h2 class="bemutatkozasDivSzoveg">Rólunk</h2>
-                    <h5 class="bemutatkozasDivSzoveg">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h5>
+                <td width="50%" class="fooldalTableTd">
+                    <h1 class="fooldalDivSzovegCim">Rólunk</h1>
+                    <h5 class="fooldalDivSzoveg">A HaLáli Kft. egy dinamikusan fejlődő webáruház, amely villanyszerelési termékek széles választékát kínálja ügyfeleinek. A cég alapítói, Halasi Martin és Lálity Dominik, több éves szakmai tapasztalattal rendelkeznek a villanyszerelés területén, és céljuk, hogy minőségi, megbízható termékeket biztosítsanak a piacon. A HaLáli Kft. webáruháza egyszerű és gyors vásárlási élményt kínál, ahol a legújabb technológiákat és eszközöket találhatják meg a szakemberek és a háztartások is. A cég elkötelezett a kiváló ügyfélszolgálat mellett, hogy minden vásárló teljes elégedettséggel távozhasson. Rendeljen kényelmesen otthonról, és bízzon a HaLáli Kft. megbízhatóságában!</h5>
+                </td>
+            </tr>
+        </table>
+    </div>
+
+    <div class="termekekDiv">
+        <table>
+            <tr>
+                <td width="50%">
+                    <img width="85%" src="./képek/izzok.jpg" alt="Izzók">
+                </td>
+                <td width="50%" class="fooldalTableTd">
+                    <h1 class="fooldalDivSzovegCim">Kínálatunk</h1>
+                    <h5 class="fooldalDivSzoveg">Fedezze fel webáruházunk gazdag választékát, ahol minden villanyszerelési eszközt és anyagot könnyedén megtalál! Nézze meg a Termékek részt, és kattintson a számos kategória egyikére, hogy gyorsan rátaláljon a szükséges eszközökre és alapanyagokra. Akár szakember, akár háztartást vezet, nálunk mindent megtalál, amire szüksége lehet a villanyszereléshez. Vásároljon egyszerűen, kényelmesen, közvetlenül online!</h5>
+                    <br><a href="./termekek.php" class="fooldalDivButton">TERMÉKEK</a>
                 </td>
             </tr>
         </table>
