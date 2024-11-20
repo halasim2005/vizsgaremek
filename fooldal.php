7d29879b2633017ae9be87d0261399515224fe04
Lálity Dominik Krisztofer <13c-lality@IPARI.SULINET.HU>
2024-11-20 11:40:51 +0100

    fooldal folyt

--- fooldal.php
+++ fooldal.php
@@ -30,9 +30,9 @@ header("Pragma: no-cache");
                 <td width="50%">
                     <img id="logoKepBemutatkozasDiv" src="./képek/HaLálip.png" alt="HaLáli Villszer Kft. logo">
                 </td>
-                <td width="50%">
-                    <h2 id="bemutatkozasDivSzoveg">Rólunk</h2>
-                    <h5 id="bemutatkozasDivSzoveg">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h5>
+                <td width="50%" class="bemutatkozoTableTd">
+                    <h2 class="bemutatkozasDivSzoveg">Rólunk</h2>
+                    <h5 class="bemutatkozasDivSzoveg">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h5>
                 </td>
             </tr>
         </table>
