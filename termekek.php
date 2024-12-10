0a265edbb522a4763ab40871d9093233e8e28a48
LDominik1 <10c-lality@ipari.vein.hu>
2024-12-10 22:03:58 +0100

    folyt

--- termekek.php
+++ termekek.php
@@ -157,14 +157,13 @@ include './db.php'; // Az adatbázis kapcsolat betöltése
                         </div>
                     </div>
                 </div>
-
             <?php
             }
             ?>
         </div>
     </div>
 
-    <script src="./szures.js"></script>
+    <script src="./js/termekek.js"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 
