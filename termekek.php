693c3b145f4878242a53957f4d7bedd2b41793f2
Halasi Martin <13c-halasi@IPARI.SULINET.HU>
2024-12-12 10:36:12 +0100

    style_add_kosar

--- termekek.php
+++ termekek.php
@@ -163,8 +163,6 @@ include './db.php'; // Az adatbázis kapcsolat betöltése
         </div>
     </div>
 
-    <script src="./szures.js"></script>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <script src="./js/termekek.js"></script>
 
