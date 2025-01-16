9491757c78e83acfc20114cd6bcb543a2d5b77e9
Halasi Martin <13c-halasi@IPARI.SULINET.HU>
2025-01-16 13:22:23 +0100

    kosar oldalon mennyiseg modositas eseten maximalizalt ertek beallitasi a raktaron elerheto mennyiseg alapjan

--- kosar.php
+++ kosar.php
@@ -60,7 +60,7 @@ function betolt_kosar_adatbazisbol($pdo) {
     if (isset($_SESSION['felhasznalo']['fh_nev'])) {
         $fh_nev = $_SESSION['felhasznalo']['fh_nev'];
         $query = "SELECT tetelek.termek_id, tetelek.tetelek_mennyiseg, termek.nev as termek_nev, 
-                            termek.egysegar, termek.kep as termek_kep
+                            termek.egysegar, termek.kep as termek_kep, termek.elerheto_darab
                     FROM tetelek
                     INNER JOIN termek ON tetelek.termek_id = termek.id
                     WHERE tetelek.fh_nev = ? AND tetelek.statusz = 'kosárban'";
@@ -291,7 +291,7 @@ $profil_teljes = $bejelentkezve ? teljes_e_a_profil($_SESSION['felhasznalo']) : 
                                         <div class="card-body">
                                             <h5 class="card-title kosarFelirat"><?= htmlspecialchars($termek['termek_nev']) ?></h5>
                                             <p class="card-text"><?= $termek['egysegar'] ?> Ft / db</p>
-                                            <input type="number" name="mennyisegek[<?= $index ?>]" value="<?= $termek['tetelek_mennyiseg'] ?>" min="0" class="form-control w-25"><br>
+                                            <input type="number" name="mennyisegek[<?= $index ?>]" value="<?= $termek['tetelek_mennyiseg'] ?>" min="0" max="<?= $termek['elerheto_darab'] ?>" class="form-control w-25"><br>
                                             <p class="card-text kosarAr"><strong><?= $termek['egysegar'] * $termek['tetelek_mennyiseg'] ?> Ft</strong></p>
                                                 <input type="hidden" name="termek_id" value="<?= htmlspecialchars($termek['termek_id']) ?>">
                                                 <button type="submit" name="delete_item" value="<?= $termek['termek_id'] ?>" class="kukaGomb" ><img class="kukaKep" src="./képek/torlesikon.svg" href="Törlés"></button>
