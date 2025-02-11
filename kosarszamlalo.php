aff34651fc8f917d58a85bf1da0d800753286723
LDominik1 <10c-lality@ipari.vein.hu>
2025-02-11 19:10:26 +0100

    termekek feltoltoget

--- kosarszamlalo.php
+++ kosarszamlalo.php
@@ -1,8 +1,18 @@
 <?php
 
-$adat = file_get_contents("kosarszamlalo.txt");
+/*$adat = file_get_contents("kosarszamlalo.txt");
 
 if (!isset($_SESSION['felhasznalo']))echo 0;
-else echo nl2br(htmlspecialchars($adat));
+else echo nl2br(htmlspecialchars($adat));*/
+
+if (!isset($_SESSION['felhasznalo'])) {
+    echo 0; 
+} else {
+    if (!isset($_SESSION['kosar']) || empty($_SESSION['kosar'])) {
+        echo 0;
+    } else {
+        echo count($_SESSION['kosar']);
+    }
+}
 
 ?>
