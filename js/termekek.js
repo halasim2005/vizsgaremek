496eacfeac8aa5e5f5216f7b12057f489bfb993f
LDominik1 <10c-lality@ipari.vein.hu>
2025-02-28 15:23:24 +0100

    termekek akcio javit

--- js/termekek.js
+++ js/termekek.js
@@ -138,7 +138,7 @@ async function termekekLeker() {
                     kartyaArszoveg = `${parseInt(termek.egysegar).toLocaleString()} Ft`;
                 }
                 
-                if(termek.akcios_ar != null && termek.akcio_kezdete != null && termek.akcio_vege != null){
+                if(termek.akcios_ar != null){
                     kartyaArszoveg = `<span style="text-decoration:line-through;color:gray;font-style:italic;margin-right:5px">${parseInt(termek.egysegar).toLocaleString()} Ft </span><span style="color:red;font-weight:bold"> ${parseInt(termek.akcios_ar).toLocaleString()} Ft</span>`;
                     let akcio = Math.floor(Math.abs((termek.akcios_ar / termek.egysegar * 100)));
                     let akcioRendes = 100 - akcio;
@@ -236,7 +236,7 @@ async function osszesTermekekLeker() {
                     kartyaArszoveg = `${parseInt(termek.egysegar).toLocaleString()} Ft`;
                 }
                 
-                if(termek.akcios_ar != null && termek.akcio_kezdete != null && termek.akcio_vege != null){
+                if(termek.akcios_ar != null){
                     kartyaArszoveg = `<span style="text-decoration:line-through;color:gray;font-style:italic;margin-right:5px">${parseInt(termek.egysegar).toLocaleString()} Ft </span><span style="color:red;font-weight:bold"> ${parseInt(termek.akcios_ar).toLocaleString()} Ft</span>`;
                     let akcio = Math.floor(Math.abs((termek.akcios_ar / termek.egysegar * 100)));
                     let akcioRendes = 100 - akcio;
