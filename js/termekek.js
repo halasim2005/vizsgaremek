2f9c6d94a6c54f605ffda20b9b545a1a2aa61059
LDominik1 <10c-lality@ipari.vein.hu>
2025-02-28 12:19:55 +0100

    termekek fix

--- js/termekek.js
+++ js/termekek.js
@@ -79,7 +79,6 @@ async function termekekLeker() {
         let maxRangeAr = document.getElementById("maxRangeAr").value;
         let kereses = document.getElementById("keresesSzures").value;
         let rendez = document.getElementById("rendez").value;
-        console.log(rendez)
 
         let bodyAdatok = {
             'kategoria': kategoria,
@@ -116,8 +115,9 @@ async function termekekLeker() {
                 }
 
                 document.getElementById("valaszSzoveg").innerHTML = ""
-                if (termek.elerheto_darab == 0) {
-                    termekekTartalom.innerHTML += `
+
+                termekekTartalom.innerHTML += 
+                `
                     <div class="col-md-4 col-sm-6 col-xs-12 mb-4">
                         <div class="card shadow">
                             <div id="termekekKartyaKepKozep">
@@ -125,7 +125,7 @@ async function termekekLeker() {
                             </div>
                             <div class="card-body">
                                 <h6 class="card-title">${termek.nev}</h6>
-                                <h6><strong><span style="color: red">A termék nincs készleten!<span></strong></h6>
+                                <h6><strong> ${(termek.elerheto_darab == 0) ? `<span style="color: red">A termék nincs készleten!</span>` : parseInt(termek.egysegar).toLocaleString() } </strong></h6>
                                 <form method="POST" action="kosar_muveletek.php">
                                     <input type="hidden" name="termek_id" value="${termek.id}">
                                     <input type="hidden" name="termek_kep" value="${termek.kep}">
@@ -137,89 +137,7 @@ async function termekekLeker() {
                             </div>
                         </div>
                     </div>
-    
-                    <!-- Modal -->
-                    <div class="modal fade" id="modal_${termek.id}" tabindex="-1" aria-labelledby="modalLabel_${termek.id}" aria-hidden="true">
-                        <div class="modal-dialog modal-xl">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="modalLabel_${termek.id}">${termek.nev}</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <img src="${termek.kep}" alt="${termek.nev}" class="img-fluid mb-3">
-                                    <p>${termek.leiras}</p>
-                                    <h6><strong>Ár: ${parseInt(termek.egysegar).toLocaleString()} Ft</strong></h6>
-                                    <p><strong>Gyártó:</strong> ${termek.gyarto}</p>
-                                    <p><strong>Kategória:</strong> ${termek.kategoria_nev}</p>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" id="modalKartyaGomb" class="btn btn-secondary" data-bs-dismiss="modal">Bezár</button>
-                                    <form method="POST" action="kosar_muveletek.php">
-                                        <input type="hidden" name="termek_id" value="${termek.t_id}">
-                                        <input type="hidden" name="termek_kep" value="${termek.kep}">
-                                        <input type="hidden" name="ar" value="${termek.egysegar}">
-                                        <input type="hidden" name="mennyiseg" value="1">
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 `;
-                } else if (termek.elerheto_darab > 0) {
-                    document.getElementById("valaszSzoveg").innerHTML = ""
-                    termekekTartalom.innerHTML += `
-                    <div class="col-md-4 col-sm-6 col-xs-12 mb-4">
-                        <div class="card shadow">
-                            <div id="termekekKartyaKepKozep">
-                                <img id="termekekKartyaKep" src="${termek.kep}" class="card-img-top" alt="${termek.t_nev}">
-                            </div>
-                            <div class="card-body">
-                                <h6 class="card-title">${termek.nev}</h6>
-                                <h6><strong>${parseInt(termek.egysegar).toLocaleString()} Ft</strong></h6>
-                                <form method="POST" action="kosar_muveletek.php">
-                                    <input type="hidden" name="termek_id" value="${termek.id}">
-                                    <input type="hidden" name="termek_kep" value="${termek.kep}">
-                                    <input type="hidden" name="ar" value="${termek.egysegar}">
-                                    <input type="hidden" name="mennyiseg" value="1">
-                                    <button type="button" id="termekekKartyaGomb" class="btn btn-primary w-100 my-1" data-bs-toggle="modal" data-bs-target="#modal_${termek.id}">Részletek</button>
-                                    <button type="submit" id="termekekKartyaGomb" name="add_to_cart" class="btn btn-primary w-100 my-1" onclick="Szamlalo()">Kosárba</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-
-    
-                    <!-- Modal -->
-                    <div class="modal fade" id="modal_${termek.id}" tabindex="-1" aria-labelledby="modalLabel_${termek.id}" aria-hidden="true">
-                        <div class="modal-dialog modal-xl">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="modalLabel_${termek.id}">${termek.nev}</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <img src="${termek.kep}" alt="${termek.nev}" class="img-fluid mb-3">
-                                    <p>${termek.leiras}</p>
-                                    <h6><strong>Ár: ${parseInt(termek.egysegar).toLocaleString()} Ft</strong></h6>
-                                    <p><strong>Gyártó:</strong> ${termek.gyarto}</p>
-                                    <p><strong>Kategória:</strong> ${termek.kategoria_nev}</p>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" id="modalKartyaGomb" class="btn btn-secondary" data-bs-dismiss="modal">Bezár</button>
-                                    <form method="POST" action="kosar_muveletek.php">
-                                        <input type="hidden" name="termek_id" value="${termek.t_id}">
-                                        <input type="hidden" name="termek_kep" value="${termek.kep}">
-                                        <input type="hidden" name="ar" value="${termek.egysegar}">
-                                        <input type="hidden" name="mennyiseg" value="1">
-                                        <button type="submit" id="termekekKartyaGomb" name="add_to_cart" class="btn btn-primary" onclick="Szamlalo()">Kosárba</button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                `;
-                }
             }
         }
     } catch (error) {
@@ -263,8 +181,8 @@ async function osszesTermekekLeker() {
 
                 }
 
-                if (termek.elerheto_darab == 0) {
-                    termekekTartalom.innerHTML += `
+                termekekTartalom.innerHTML += 
+                `
                     <div class="col-md-4 col-sm-6 col-xs-12 mb-4">
                         <div class="card shadow">
                             <div id="termekekKartyaKepKozep">
@@ -272,7 +190,7 @@ async function osszesTermekekLeker() {
                             </div>
                             <div class="card-body">
                                 <h6 class="card-title">${termek.nev}</h6>
-                                <h6><strong> <span style="color: red">A termék nincs készleten!<span></strong></h6>
+                                <h6><strong> ${(termek.elerheto_darab == 0) ? `<span style="color: red">A termék nincs készleten!</span>` : parseInt(termek.egysegar).toLocaleString() } </strong></h6>
                                 <form method="POST" action="kosar_muveletek.php">
                                     <input type="hidden" name="termek_id" value="${termek.id}">
                                     <input type="hidden" name="termek_kep" value="${termek.kep}">
@@ -284,101 +202,7 @@ async function osszesTermekekLeker() {
                             </div>
                         </div>
                     </div>
-    
-                    <!-- Modal -->
-                    <div class="modal fade" id="modal_${termek.id}" tabindex="-1" aria-labelledby="modalLabel_${termek.id}" aria-hidden="true">
-                        <div class="modal-dialog modal-xl">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="modalLabel_${termek.id}">${termek.nev}</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <img src="${termek.kep}" alt="${termek.nev}" class="img-fluid mb-3">
-                                    <p>${termek.leiras}</p>
-                                    <h6><strong>Ár: ${parseInt(termek.egysegar).toLocaleString()} Ft</strong></h6>
-                                    <p><strong>Gyártó:</strong> ${termek.gyarto}</p>
-                                    <p><strong>Kategória:</strong> ${termek.kategoria_nev}</p>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" id="modalKartyaGomb" class="btn btn-secondary" data-bs-dismiss="modal">Bezár</button>
-                                    <form method="POST" action="kosar_muveletek.php">
-                                        <input type="hidden" name="termek_id" value="${termek.t_id}">
-                                        <input type="hidden" name="termek_kep" value="${termek.kep}">
-                                        <input type="hidden" name="ar" value="${termek.egysegar}">
-                                        <input type="hidden" name="mennyiseg" value="1">
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 `;
-                } else if (termek.elerheto_darab > 0) {
-                    termekekTartalom.innerHTML += `
-                    <div class="col-md-4 col-sm-6 col-xs-12 mb-4">
-                        <div class="card shadow">
-                            <div id="termekekKartyaKepKozep">
-                                <img id="termekekKartyaKep" src="${termek.kep}" class="card-img-top" alt="${termek.t_nev}">
-                            </div>
-                            <div class="card-body">
-                                <h6 class="card-title">${termek.nev}</h6>
-                                <h6><strong>${parseInt(termek.egysegar).toLocaleString()} Ft</strong></h6>
-                                <form method="POST" action="kosar_muveletek.php">
-                                    <input type="hidden" name="termek_id" value="${termek.id}">
-                                    <input type="hidden" name="termek_kep" value="${termek.kep}">
-                                    <input type="hidden" name="ar" value="${termek.egysegar}">
-                                    <input type="hidden" name="mennyiseg" value="1">
-                                    <button type="button" id="termekekKartyaGomb" class="btn btn-primary w-100 my-1" data-bs-toggle="modal" data-bs-target="#modal_${termek.id}">Részletek</button>
-                                    <button type="submit" id="termekekKartyaGomb" name="add_to_cart" class="btn btn-primary w-100 my-1" onclick="Szamlalo()">Kosárba</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- Modal -->
-                    <div class="modal fade" id="modal_${termek.id}" tabindex="-1" aria-labelledby="modalLabel_${termek.id}" aria-hidden="true">
-                        <div class="modal-dialog modal-xl">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="modalLabel_${termek.id}">${termek.nev}</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <div class="container">
-                                        <!-- Első sor -->
-                                        <div class="row">
-                                            <div class="col-md-6 text-white">
-                                                <img src="${termek.kep}" alt="${termek.nev}" class="img-fluid mb-3" style="width: 65%">
-                                            </div>
-                                            <div class="col-md-6">
-                                                <h3><strong>Ár: ${parseInt(termek.egysegar).toLocaleString()} Ft</strong></h3>
-                                                <h4><strong>Gyártó:</strong> ${termek.gyarto}</h4>
-                                                <h4><strong>Kategória:</strong> ${termek.kategoria_nev}</h4>
-                                            </div>
-                                        </div>
-                                        <!-- Második sor -->
-                                        <div class="row mt-3">
-                                            <div class="col-12">
-                                                <p>${termek.leiras}</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" id="modalKartyaGomb" class="btn btn-secondary" data-bs-dismiss="modal">Bezár</button>
-                                    <form method="POST" action="kosar_muveletek.php">
-                                        <input type="hidden" name="termek_id" value="${termek.t_id}">
-                                        <input type="hidden" name="termek_kep" value="${termek.kep}">
-                                        <input type="hidden" name="ar" value="${termek.egysegar}">
-                                        <input type="hidden" name="mennyiseg" value="1">
-                                        <button type="submit" id="termekekKartyaGomb" name="add_to_cart" class="btn btn-primary" onclick="Szamlalo()">Kosárba</button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                `;
-                }
             }
         }
     } catch (error) {
