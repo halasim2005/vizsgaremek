c56b448611a9f0668ac5dc854684e0a01b08baeb
LDominik1 <10c-lality@ipari.vein.hu>
2025-03-07 20:50:59 +0100

    termekek reszletek

---
+++ js/termek_reszletek.js
@@ -0,0 +1,77 @@
+async function termek_reszletei() {
+    let urlParameterek = new URLSearchParams(window.location.search);
+    let termekId = urlParameterek.get("id");
+    
+    let adatok = await fetch("./termekek_adatok.php/termek", {
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json"
+        },
+        body : JSON.stringify({
+            "id" : termekId,
+        })
+    })
+    let termek_reszletei = await adatok.json();
+
+    for (let adat of termek_reszletei) {
+        let raktaronSzoveg;
+        let raktaronStyle;
+        let akcioSzazalek;
+        let akciosAr;
+
+        if(adat.akcios_ar != null && adat.elerheto_darab != 0){
+            let akcio = Math.floor(Math.abs((adat.akcios_ar / adat.egysegar * 100)));
+            let akcioRendes = 100 - akcio;
+            akcioSzazalek = `<h4 style="color:red;font-weight:bold">-${akcioRendes}%-os AKCIÓ!</h4>`;
+            akciosAr = `<span style="font-weight:bold">Ár:</span> <span style="text-decoration:line-through;color:gray;font-style:italic;margin-right:5px">${parseInt(adat.egysegar).toLocaleString()} Ft</span><span style="color:red;font-weight:bold">${parseInt(adat.akcios_ar).toLocaleString()} Ft</span>`;
+        }else{
+            akcioSzazalek = "";
+            akciosAr = `<span style="font-weight:bold">Ár:</span> <span>${parseInt(adat.egysegar).toLocaleString()} Ft</span>`;
+        }
+
+        if(adat.elerheto_darab > 0){
+            raktaronSzoveg = adat.elerheto_darab;
+            raktaronStyle = `style="color:green;font-weight:bold"`;
+        }else{
+            raktaronSzoveg = `Nincs a készleten!`;
+            raktaronStyle = `style="color:red;font-weight:bold"`;
+        }
+
+        document.getElementById("reszletek").innerHTML = 
+        `
+            <div class="row">
+                <div class="col-12">
+                    <h4>${adat.nev}</h4>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 col-12 p-3">
+                    ${akcioSzazalek}
+                    <img width="230px" src="${adat.kep}">
+                </div>
+                <div class="col-md-8 col-12 p-3">
+                    <h5><span style="font-weight:bold">Gyártó:</span> ${adat.gyarto}</h5>
+                    <h5><span style="font-weight:bold">Típus:</span> ${adat.tipus}</h5>
+                    <h5><span style="font-weight:bold">Raktáron:</span> <span ${raktaronStyle}>${raktaronSzoveg} db</h5>
+                    <h5>${akciosAr}</h5>
+                    <form method="POST" action="kosar_muveletek.php">
+                        <input type="hidden" name="termek_id" value="${adat.id}">
+                        <input type="hidden" name="termek_kep" value="${adat.kep}">
+                        <input type="hidden" name="ar" value="${adat.egysegar}">
+                        <input type="hidden" name="mennyiseg" value="1">
+                        <button type="submit" ${(adat.elerheto_darab == 0) ? `disabled` : ``} style="width:160px" id="termekekKartyaKosarGomb" name="add_to_cart" class="btn btn-primary my-1" onclick="Szamlalo()">Kosárba</button>
+                    </form>
+                </div>
+            </div>
+            <div class="row mb-5">
+                <div class="col-12 p-3">
+                    <h5>${adat.leiras}</h5>
+                </div>
+            </div>
+        `
+    }
+
+    
+}
+
+window.addEventListener("load", termek_reszletei);
---
+++ termek_reszletek.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Pragma: no-cache");
+
+include './db.php';
+?>
+
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&family=Kanit:wght@300&family=Montserrat&family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./style/style.css">
+    <title>Termék részletei</title>
+</head>
+<body>
+
+    <?php
+        include './nav.php';
+    ?>
+
+    <div class="container mt-5" id="reszletek">
+        
+    </div>
+
+    <?php
+        include './footer.php';
+    ?>
+
+    <script src="./js/termek_reszletek.js"></script>
+</body>
+</html>
